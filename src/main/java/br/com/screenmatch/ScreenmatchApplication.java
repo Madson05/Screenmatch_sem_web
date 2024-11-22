27a3a3b5c4588266c42121b0248b9fcb1462b780
Madson Silva <madsonsilva2005@gmail.com>
2024-11-22 11:19:03 -0300

    inicio do projeto

---
+++ src/main/java/br/com/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.screenmatch;
+
+import br.com.screenmatch.models.DadosSerie;
+import br.com.screenmatch.service.ConsumoApi;
+import br.com.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados(("https://www.omdbapi.com/?t=gilmore+girls&season=1&&apikey=7aec031b"));
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
