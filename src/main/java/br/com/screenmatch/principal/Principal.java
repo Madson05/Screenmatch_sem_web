89c7ec8ebbd504ea07f9172b37c0425fa722f902
Madson Silva <madsonsilva2005@gmail.com>
2024-11-24 20:04:59 -0300

    adicionada funcionalidade de listagem de temporaadas na classe principal, abstraindo-a da aplicação pprincipal

--- src/main/java/br/com/screenmatch/principal/Principal.java
+++ src/main/java/br/com/screenmatch/principal/Principal.java
@@ -1,20 +1,38 @@
 package br.com.screenmatch.principal;
 
+import br.com.screenmatch.models.DadosSerie;
+import br.com.screenmatch.models.DadosTemporada;
 import br.com.screenmatch.service.ConsumoApi;
+import br.com.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner scanner = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
+    ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=7aec031b";
     public void exibemenu(){
         System.out.println("Digite o nome da série: ");
         var serie = scanner.nextLine();
 
-        var json = consumoApi.obterDados(ENDERECO.replace(" ", "+") + serie + API_KEY);
+        var json = consumoApi.obterDados(ENDERECO + serie.replace(" ", "+") + API_KEY);
+
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados((ENDERECO + serie.replace(" ", "+") + "&season=" + i + API_KEY));
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
 
     }
 
